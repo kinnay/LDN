15ab244703eb949be9d7b24da95a26336308c8e9
Yannik Marchand <ymarchand@me.com>
2022-03-27 18:54:01 +0200

    Push new version to pip to fix missing pycryptodome dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = \
 
 setuptools.setup(
 	name = "ldn",
-	version = "0.0.4",
+	version = "0.0.5",
 	description = description,
 	long_description = long_description,
 	author = "Yannik Marchand",
