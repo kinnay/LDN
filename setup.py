c16aa4c259ae2d09cbe38bcc6c9730c53539d980
Yannik Marchand <ymarchand@me.com>
2024-01-01 22:25:24 +0100

    Release 0.0.9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = \
 
 setuptools.setup(
 	name = "ldn",
-	version = "0.0.8",
+	version = "0.0.9",
 	description = description,
 	long_description = long_description,
 	author = "Yannik Marchand",
